0fc31e8f03d4e1bcef0186105164e820153b4466
hirose <hirononari@gmail.com>
2020-11-09 14:02:59 +0900

    くじ追加

---
+++ java/20201109/Sclatch.java
@@ -0,0 +1,32 @@
+public class Sclatch{
+	public static void main(String[] args){
+		int sum=0;
+		for(int i=0;i<3;i++){
+			System.out.printf("%d回目%n",i+1);
+		int n=new java.util.Random().nextInt(100)+1;
+			System.out.println(n);
+		if(n==1){
+			sum+=10000000;
+			System.out.println("1000万円");
+		}else if(n<=3){
+			sum+=1000000;
+			System.out.println("100万円");
+		}else if(n<=6){
+			sum+=100000;
+			System.out.println("10万円");
+		}else if(n<=10){
+			sum+=10000;
+			System.out.println("1万円");
+		}else if(n<=20){
+			sum+=1000;
+			System.out.println("1000円");
+		}else if(n<=40){
+			sum+=100;
+			System.out.println("100円");
+		}else{
+			System.out.println("ハズレ");
+		}
+		}
+		System.out.printf("獲得賞金は%n%,d円でした！",sum);
+	}
+}
