51ecb2620111b9d1d876eb27d864759715efe8cb
hirose <hirononari@gmail.com>
2020-11-10 09:42:16 +0900

    ArrLesson修正

--- java/20201109/ArrLesson3.java
+++ java/20201109/ArrLesson3.java
@@ -6,9 +6,17 @@ public class ArrLesson3{
 		for (int i=0;i<detaA.length;i++){
 			System.out.print(detaA[i]);
 		}
+		//拡張for文detaBの要素を未読が無くなるまで全部取り出す
 		for (int n : detaB){
 			System.out.println(n);
 		}
 		System.out.println(Arrays.toString(detaA));
 	}
 }
+//String[] strs={hoge,fuga,bar};
+//for (int i=0;i<strs.length;i++){
+//System.out.println(strs[i]);
+//}
+//for (String words : strs){
+//System.out.println(words);
+//}
